e7f6bdd18426db1ef599d41385aeba132ed57de2
KoblizekXD <janprokupek@centrum.cz>
2024-11-24 20:17:08 +0100

    Add run-task plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("io.papermc.paperweight.userdev") version "1.7.5"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
 }
 
 group = "lol.koblizek"
@@ -14,6 +15,10 @@ dependencies {
     paperweight.paperDevBundle("1.21.3-R0.1-SNAPSHOT")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.runServer {
+    minecraftVersion("1.21.3")
 }
 
 tasks.test {
