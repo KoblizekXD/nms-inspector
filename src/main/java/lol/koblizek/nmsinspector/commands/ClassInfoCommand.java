779c5f9660fb46ccb4dbcfb4e5a2b7d950ec9413
KoblizekXD <janprokupek@centrum.cz>
2024-11-25 11:06:23 +0100

    Add filter support & constructors, fields, methods

--- src/main/java/lol/koblizek/nmsinspector/commands/ClassInfoCommand.java
+++ src/main/java/lol/koblizek/nmsinspector/commands/ClassInfoCommand.java
@@ -8,16 +8,22 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Optional;
+
 public class ClassInfoCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (args.length == 0) return false;
-
+        
+        Filter filter = Filter.NONE;
+        if (args.length == 2)
+            filter = Filter.getValue(args[1]).orElse(Filter.NONE);
+        
         try {
             Class<?> type = Class.forName(args[0]);
             
             Component classInfo = Component.empty()
-                    .append(ClassInfoBuilder.getSummary(type))
+                    .append(ClassInfoBuilder.getSummary(type, filter))
                     .appendNewline();
             sender.sendMessage(classInfo);
         } catch (ClassNotFoundException e) {
@@ -26,5 +32,32 @@ public class ClassInfoCommand implements CommandExecutor {
         }
 
         return true;
+    }
+    
+    public enum Filter {
+        ALL("*"),
+        FIELDS("fields"),
+        METHODS("methods"),
+        CONSTRUCTORS("constructors"),
+        NONE("none");
+        
+        private final String str;
+
+        Filter(String filter) {
+            this.str = filter;
+        }
+
+        @Override
+        public String toString() {
+            return str;
+        }
+        
+        public static Optional<Filter> getValue(String filter) {
+            for (Filter f : values()) {
+                if (f.str.equals(filter))
+                    return Optional.of(f);
+            }
+            return Optional.empty();
+        }
     }
 }
--- src/main/java/lol/koblizek/nmsinspector/util/ClassInfoBuilder.java
+++ src/main/java/lol/koblizek/nmsinspector/util/ClassInfoBuilder.java
@@ -1,11 +1,13 @@
 package lol.koblizek.nmsinspector.util;
 
+import lol.koblizek.nmsinspector.commands.ClassInfoCommand;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.JoinConfiguration;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.text.format.TextDecoration;
 
+import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
 
@@ -13,17 +15,37 @@ public final class ClassInfoBuilder {
     
     private ClassInfoBuilder() {}
     
-    public static Component getSummary(Class<?> type) {
-        return Component.empty()
+    public static Component getSummary(Class<?> type, ClassInfoCommand.Filter filter) {
+        var component = Component.empty()
+                .append(getAnnotations(type))
+                .appendNewline()
                 .append(modsAsComponent(type.getModifiers()))
                 .appendSpace()
                 .append(Component.text("class").color(NamedColors.KEYWORDS))
                 .appendSpace()
-                .append(Component.text(type.getSimpleName()).color(NamedColors.SYMBOLS))
+                .append(Component.text(type.getSimpleName()).color(NamedColors.SYMBOLS)
+                        .hoverEvent(HoverEvent.showText(Component.text(type.getName()).color(NamedColors.SYMBOLS))))
                 .append(getSupers(type))
                 .append(Component.text(";").color(NamedColors.UNUSED))
                 .appendNewline()
-                .append(getMemberSummary(type)); 
+                .append(getMemberSummary(type));
+        
+        if (filter == ClassInfoCommand.Filter.FIELDS || filter == ClassInfoCommand.Filter.ALL)
+            component = component.appendNewline().append(getFields(type));
+        
+        if (filter == ClassInfoCommand.Filter.CONSTRUCTORS || filter == ClassInfoCommand.Filter.ALL)
+            component = component.appendNewline().append(getConstructors(type));
+        
+        if (filter == ClassInfoCommand.Filter.METHODS || filter == ClassInfoCommand.Filter.ALL)
+            component = component.appendNewline().append(getMethods(type));
+        
+        return component;
+    }
+    
+    public static Component getAnnotations(AnnotatedElement element) {
+        return Component.join(JoinConfiguration.separator(Component.newline()), Arrays.stream(element.getAnnotations())
+                .map(a -> Component.text("@" + a.annotationType().getSimpleName()).color(NamedColors.SYMBOLS))
+                .toList());
     }
     
     private static Component getSupers(Class<?> type) {
@@ -61,5 +83,44 @@ public final class ClassInfoBuilder {
     public static Component modsAsComponent(int modifiers) {
         return Component.text(Modifier.toString(modifiers))
                 .color(NamedColors.KEYWORDS);
+    }
+    
+    public static Component getFields(Class<?> type) {
+        return Component.join(JoinConfiguration.separator(Component.newline()), Arrays.stream(type.getDeclaredFields()).map(field -> Component.empty()
+                .append(modsAsComponent(field.getModifiers()))
+                .appendSpace().append(Component.text(field.getType().getSimpleName()).color(NamedColors.SYMBOLS).clickEvent(ClickEvent.runCommand("/classinfo " + field.getType().getName()))
+                        .hoverEvent(HoverEvent.showText(Component.text("Click to view " + field.getType().getName()).color(NamedColors.SYMBOLS)))
+                        .decorate(TextDecoration.UNDERLINED))
+                .appendSpace().append(Component.text(field.getName()).color(NamedColors.SYMBOLS))
+                .append(Component.text(";").color(NamedColors.UNUSED))).toList());
+    }
+
+    public static Component getMethods(Class<?> type) {
+        return Component.join(JoinConfiguration.separator(Component.newline()), Arrays.stream(type.getDeclaredMethods()).map(method -> Component.empty()
+                .append(modsAsComponent(method.getModifiers()))
+                .appendSpace().append(Component.text(method.getReturnType().getSimpleName()).color(NamedColors.SYMBOLS).clickEvent(ClickEvent.runCommand("/classinfo " + method.getReturnType().getName()))
+                        .hoverEvent(HoverEvent.showText(Component.text("Click to view " + method.getReturnType().getName()).color(NamedColors.SYMBOLS)))
+                        .decorate(TextDecoration.UNDERLINED))
+                .appendSpace().append(Component.text(method.getName()).color(NamedColors.SYMBOLS))
+                .append(Component.text("(").color(NamedColors.UNUSED))
+                .append(Component.join(JoinConfiguration.separator(Component.text(", ").color(NamedColors.UNUSED)), Arrays.stream(method.getParameterTypes())
+                        .map(c -> Component.text(c.getSimpleName()).color(NamedColors.SYMBOLS).clickEvent(ClickEvent.runCommand("/classinfo " + c.getName()))
+                                .hoverEvent(HoverEvent.showText(Component.text("Click to view " + c.getName()).color(NamedColors.SYMBOLS)))
+                                .decorate(TextDecoration.UNDERLINED)).toList()))
+                .append(Component.text(")").color(NamedColors.UNUSED))
+                .append(Component.text(";").color(NamedColors.UNUSED))).toList());
+    }
+
+    public static Component getConstructors(Class<?> type) {
+        return Component.join(JoinConfiguration.separator(Component.newline()), Arrays.stream(type.getDeclaredConstructors()).map(method -> Component.empty()
+                .append(modsAsComponent(method.getModifiers()))
+                .appendSpace().append(Component.text(type.getSimpleName()).color(NamedColors.SYMBOLS))
+                .append(Component.text("(").color(NamedColors.UNUSED))
+                .append(Component.join(JoinConfiguration.separator(Component.text(", ").color(NamedColors.UNUSED)), Arrays.stream(method.getParameterTypes())
+                        .map(c -> Component.text(c.getSimpleName()).color(NamedColors.SYMBOLS).clickEvent(ClickEvent.runCommand("/classinfo " + c.getName()))
+                                .hoverEvent(HoverEvent.showText(Component.text("Click to view " + c.getName()).color(NamedColors.SYMBOLS)))
+                                .decorate(TextDecoration.UNDERLINED)).toList()))
+                .append(Component.text(")").color(NamedColors.UNUSED))
+                .append(Component.text(";").color(NamedColors.UNUSED))).toList());
     }
 }
